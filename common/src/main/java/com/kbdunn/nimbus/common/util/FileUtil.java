98443a710e84f698d6c8ed5eb41825b33d85b1df
Bryson Dunn <k.bryson.dunn@gmail.com>
2016-05-03 15:21:51 -0600

    Add method to relativize paths

--- common/src/main/java/com/kbdunn/nimbus/common/util/FileUtil.java
+++ common/src/main/java/com/kbdunn/nimbus/common/util/FileUtil.java
@@ -1,9 +1,15 @@
 package com.kbdunn.nimbus.common.util;
 
+import java.io.File;
+
 public class FileUtil {
 
 	public static final String INVALID_FILENAME_CHARACTERS = "/\\?%*:|\"<>";
 	public static final String INVALID_PATH_CHARACTERS = "\\?%*:|\"<>";
+	
+	private FileUtil() {
+		// Only static methods
+	}
 	
 	public static boolean pathContainsInvalidCharacters(String path) {
 		return StringUtil.stringContainsInvalidCharacters(path, INVALID_PATH_CHARACTERS);
@@ -11,5 +17,9 @@ public class FileUtil {
 	
 	public static boolean filenameContainsInvalidCharacters(String name) {
 		return StringUtil.stringContainsInvalidCharacters(name, INVALID_FILENAME_CHARACTERS);
+	}
+	
+	public static File relativize(File base, File child) {
+		return new File(base.toURI().relativize(child.toURI()).getPath());
 	}
 }
